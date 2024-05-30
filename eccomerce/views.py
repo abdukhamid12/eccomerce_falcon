42c3262a7e4987d9f21085f97aee2b8556a4ef2d
Smoke_Front_Back_Developing <116000706+abdukhamid12@users.noreply.github.com>
2024-05-30 18:10:03 +0500

    18-09

--- eccomerce/views.py
+++ eccomerce/views.py
@@ -1,10 +1,8 @@
 from django.shortcuts import render
 
-from eccomerce.models import Product
 
 
 # Create your views here.
 def store(request):
-    products = Product.objects.all()
-    context = {'products': products}
-    return render(request, 'store/e-commerce/product/product-details.html', context)
+    context = {}
+    return render(request, 'base.html', context)
