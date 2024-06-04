eab9a8bfb3a57fce089701347e8a19ee7619ab5d
Smoke_Front_Back_Developing <116000706+abdukhamid12@users.noreply.github.com>
2024-06-04 17:29:15 +0500

    17:28 04.06.2024

--- eccomerce/urls.py
+++ eccomerce/urls.py
@@ -5,4 +5,6 @@ from eccomerce import views
 app_name = 'store'
 urlpatterns = [
     path('', views.store, name='store'),
+    path('product_list/', views.product_list, name='product_list'),
+    path('product_details/', views.product_details, name='product_details'),
 ]
--- eccomerce/views.py
+++ eccomerce/views.py
@@ -5,4 +5,15 @@ from django.shortcuts import render
 # Create your views here.
 def store(request):
     context = {}
+    # return render(request, 'store/e-commerce/product/product-list.html', context)
+    # return render(request, 'store/e-commerce/product/product-details.html', context)
+    return render(request, 'store/e-commerce/product/product-grid.html', context)
+    # return render(request, 'base.html', context)
+
+def product_list(request):
+    context = {}
     return render(request, 'store/e-commerce/product/product-list.html', context)
+
+def product_details(request):
+    context = {}
+    return render(request, 'store/e-commerce/product/product-details.html', context)
