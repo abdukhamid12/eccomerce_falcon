0b8e0bb65c48431aa705319d2ce4da65aa219b85
Smoke_Front_Back_Developing <116000706+abdukhamid12@users.noreply.github.com>
2024-06-15 18:16:32 +0500

    18:16

--- eccomerce/models.py
+++ eccomerce/models.py
@@ -30,8 +30,8 @@ class Product(TimeStampedModel):
     description = models.TextField(null=True, blank=True)
     image = models.ImageField(upload_to='product_img/', blank=True, null=True)
     price = models.DecimalField(decimal_places=2, max_digits=8)
-    digital = models.BooleanField(default=False, null=True, blank=True)
     info = models.TextField(max_length=255, null=True, blank=True)
+    size = models.CharField(max_length=100, null=True, blank=True)
 
     def __str__(self):
         return self.name
--- eccomerce/views.py
+++ eccomerce/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 
 from eccomerce.models import *
 
@@ -12,43 +12,55 @@ def store(request):
     return render(request, 'store/e-commerce/product/product-grid.html', context)
     # return render(request, 'base.html', context)
 
+
 def product_list(request):
     products = Product.objects.all()
     context = {'products': products}
     return render(request, 'store/e-commerce/product/product-list.html', context)
 
-def product_details(request):
-    products = Product.objects.all()
-    context = {'products': products}
+
+def product_details(request, pk):
+    product = get_object_or_404(Product, pk=pk)
+    context = {
+        "product": product
+    }
     return render(request, 'store/e-commerce/product/product-details.html', context)
+
 
 def order_list(request):
     context = {}
     return render(request, 'store/e-commerce/orders/order-list.html', context)
 
+
 def order_details(request):
     context = {}
     return render(request, 'store/e-commerce/orders/order-details.html', context)
+
 
 def customers(request):
     context = {}
     return render(request, 'store/e-commerce/customers.html', context)
 
+
 def customers_details(request):
     context = {}
     return render(request, 'store/e-commerce/customer-details.html', context)
+
 
 def shopping_cart(request):
     context = {}
     return render(request, 'store/e-commerce/shopping-cart.html', context)
 
+
 def checkout(request):
     context = {}
     return render(request, 'store/e-commerce/checkout.html', context)
 
+
 def billing(request):
     context = {}
     return render(request, 'store/e-commerce/billing.html', context)
+
 
 def invoice(request):
     context = {}
